a4ee0084f80bc2cfdc05f7f43a009a0297bbd573
Gonzalo Lorca <2238976l@student.gla.ac.uk>
2021-07-27 14:30:08 +0100

    up to part 4.4. Before doing the exercises, don't understand media files yet.

--- rango/urls.py
+++ rango/urls.py
@@ -5,7 +5,6 @@ app_name:'rango'
 
 urlpatterns = [
     path('index',views.index,name='index'),
-    path('gonzotriesathing',views.index,name="gonzo'sThing"),
     path ('about',views.about,name='about'),
 ]
 
--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse ("Rango says her there partner!  <a href='/rango/about'>index </a>")
+    context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cumpcake!'}
+
+    return render (request, 'rango/index.html',context=context_dict)
 
 def about(request):
     return HttpResponse("Rango says this is the about page  <a href='/rango/index'> about </a>")
