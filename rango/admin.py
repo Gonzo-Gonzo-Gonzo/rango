59df0ef4eae1ec0670c6893781ec5aa30e164095
Gonzo-Gonzo-Gonzo <76164683+Gonzo-Gonzo-Gonzo@users.noreply.github.com>
2021-07-29 10:19:43 +0100

    Chapter 5 exercises complete

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from rango.models import Category,Page
 
-admin.site.register(Category)
-admin.site.register(Page)
-# Register your models here.
+class PageInLine (admin.TabularInline):
+    model=Page
+    extra = 3
+
+class CategoryAdmin (admin.ModelAdmin):
+    fields = ['name','views','likes']
+    inlines = [PageInLine]
+    list_display = ('name','views','likes')
+
+admin.site.register(Category,CategoryAdmin)
+
+class PageAdmin (admin.ModelAdmin):
+    fields = ['title','category','url']
+    list_display = ['title','category','url']
+
+admin.site.register(Page,PageAdmin)
+
--- rango/models.py
+++ rango/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class Category (models.Model):
     name = models.CharField(max_length=128, unique=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.name
+        return self.name + str(self.likes) + str(self.views)
 
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
