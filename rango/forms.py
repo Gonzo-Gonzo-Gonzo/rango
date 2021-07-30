9bcfa83dbe8e8e9a2aaec4a6362ef084798c8a8c
Gonzo-Gonzo-Gonzo <76164683+Gonzo-Gonzo-Gonzo@users.noreply.github.com>
2021-07-30 16:44:44 +0100

    Commiting Chapter 9 before creating the login view

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
+
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.Max_Length_title, help_text="Please enter category name.") 
@@ -21,5 +23,18 @@ class PageForm (forms.ModelForm):
     class Meta :
         model=Page
         exclude = ('category',)
+
+class UserForm (forms.ModelForm):
+    pasword = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields =('username','email','password')
+
+class UserProfileForm (forms.ModelForm):
+    class Meta:
+        model =UserProfile
+        fields = ('website','picture')
+
 
 
